dda099763f5c3ac7ef0c35137ad9d7eefd6da434
Daivy03 <reale805@gmail.com>
2023-04-17 23:04:46 +0200

    Remove material navbar & Add pages

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,21 @@ void main() {
   runApp(const DemiProf());
 }
 
-class DemiProf extends StatelessWidget {
-  const DemiProf({super.key});
+class DemiProf extends StatefulWidget {
+  const DemiProf({Key? key}) : super(key: key);
+
+  @override
+  _DemiProfState createState() => _DemiProfState();
+}
+
+class _DemiProfState extends State<DemiProf> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const SearchPage(),
+    const ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,43 +29,84 @@ class DemiProf extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-            forceMaterialTransparency: true,
-            leading: Image.asset(
-              'lib/icons/dmp_logo30.png',
+          forceMaterialTransparency: true,
+          leading: Image.asset(
+            'lib/icons/dmp_logo30.png',
+          ),
+          title: RichText(
+            text: TextSpan(
+              style: GoogleFonts.poppins(
+                  fontSize: 20, fontWeight: FontWeight.bold),
+              children: const [
+                TextSpan(text: "DEMI"),
+                TextSpan(
+                  text: "PROF",
+                  style: TextStyle(color: AppColors.accent),
+                )
+              ],
             ),
-            title: RichText(
-                text: TextSpan(
-                    style: GoogleFonts.poppins(
-                        fontSize: 20, fontWeight: FontWeight.bold),
-                    children: const [
-                  TextSpan(text: "DEMI"),
-                  TextSpan(
-                    text: "PROF",
-                    style: TextStyle(color: AppColors.accent),
-                  )
-                ]))),
-        body: const Text('Ciao'),
-        bottomNavigationBar: NavigationBarTheme(
-          data: const NavigationBarThemeData(
-              height: 55,
-              backgroundColor: Colors.black,
-              indicatorColor: AppColors.accent,
-              iconTheme:
-                  MaterialStatePropertyAll(IconThemeData(color: Colors.white)),
-              labelTextStyle:
-                  MaterialStatePropertyAll(TextStyle(color: Colors.white))),
-          child: NavigationBar(
-            destinations: const [
-              NavigationDestination(
-                  icon: Icon(Icons.home_filled), label: "Home"),
-              NavigationDestination(
-                  icon: Icon(Icons.search_rounded), label: "Cerca"),
-              NavigationDestination(
-                  icon: Icon(Icons.person_outline_rounded), label: "Profilo"),
-            ],
           ),
         ),
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: AppColors.accent,
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search_rounded),
+              label: 'Cerca',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_rounded),
+              label: 'Profilo',
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Page'),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Search Page'),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile Page'),
     );
   }
 }
